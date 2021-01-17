55ee09c6e4d71d07656f581a0810931a5d27f048
Ibney Ali <ali@liberintechnologies.com>
2021-01-17 17:39:19 +0530

    update images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -20,6 +20,12 @@ function Cards() {
                         text="Travel through the Islands of Bali in a Private Cruise"
                         label='Luxury'
                         path='/services' />
+
+                        <CardItem 
+                        src="images/img-2.jpg"
+                        text="Travel through the Islands of Bali in a Private Cruise"
+                        label='Luxury'
+                        path='/services' />
                    </ul>
                    <ul className="cards__items">
                         <CardItem 
@@ -42,8 +48,21 @@ function Cards() {
                         path='/services' />
                    </ul>
                    <ul className="cards__items">
+                   <CardItem 
+                        src="images/img-3.jpg"
+                        text="Explore the hidden waterfall deep
+                        inside the Amazon Jungle"
+                        label='Adventure'
+                        path='/services' />
+
                         <CardItem 
-                        src="images/img-10.png"
+                        src="images/img-4.jpg"
+                        text="Travel through the Islands of Bali in a Private Cruise"
+                        label='Luxury'
+                        path='/services' />
+
+                        <CardItem 
+                        src="images/img-8.jpg"
                         text="Travel through the Islands of Bali in a Private Cruise"
                         label='Luxury'
                         path='/services' />
