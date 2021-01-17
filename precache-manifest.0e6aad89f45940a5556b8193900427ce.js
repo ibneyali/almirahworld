738b2c5fd487df7ed0e4f5b1c738e8b9cc8a517b
Ibney Ali <ali@liberintechnologies.com>
2021-01-17 17:19:30 +0530

    Updates

---
+++ precache-manifest.0e6aad89f45940a5556b8193900427ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6840893609f6189ef8e1de07ff089ef",
+    "url": "/naseerialmirah/index.html"
+  },
+  {
+    "revision": "0241fe6d5d3b26991c0f",
+    "url": "/naseerialmirah/static/css/main.e2c8a606.chunk.css"
+  },
+  {
+    "revision": "d67c23fc92fc8cdd8976",
+    "url": "/naseerialmirah/static/js/2.94c525c1.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/naseerialmirah/static/js/2.94c525c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0241fe6d5d3b26991c0f",
+    "url": "/naseerialmirah/static/js/main.f531e2c3.chunk.js"
+  },
+  {
+    "revision": "d908f293feaa07d32fdd",
+    "url": "/naseerialmirah/static/js/runtime-main.2a781cee.js"
+  }
+]);
